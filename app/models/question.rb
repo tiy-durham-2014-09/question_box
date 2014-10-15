e37f339c87d9d3557c03b532e2e16c8d6312ec45
GeoffPurdy <geoff_purdy@yahoo.com>
2014-10-15 15:56:51 -0400

    added other side of HABTM relationship w/ Tag to Question (left out of earlier commit

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
+  has_and_belongs_to_many :tags
 
   validates :title, presence: true
   validates :text, presence: true
