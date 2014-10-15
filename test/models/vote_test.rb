7636b309f24a169157de3d6072730215b46a33ff
Noah Holmes <noah.r.holmes@gmail.com>
2014-10-15 16:44:51 -0400

    added test names to vote/test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -9,6 +9,16 @@ class VoteTest < ActiveSupport::TestCase
     check_presence(@vote, :user)
   end
 
-  
+  test "should have a positive or negative value" do
+  end
+
+  test "should award 10 points to user of question/answer when voted positive" do
+  end
+
+  test "should subtract 5 points from user of question/answer when voted negative" do
+  end
+
+  test "should subtract 1 from user who casted vote when vote is negative" do
+  end
 
 end
