cce88e548e1d747ecafe020883079e786cb0fcbe
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-10-15 16:51:58 -0400

    Added positive vote that awards points

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,18 @@
 class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
-
   belongs_to :user
+
+  validates :value, presence: true
+  validates :value, inclusion: { in: [1, 2] }
+  validates :user, presence: true
+
+  after_save :give_points_to_recieving_user
+
+  def give_points_to_recieving_user
+    if value == 1
+      user.score += 10
+      user.save
+    end
+  end
+
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @vote = Vote.new
+  end
+
+  test "Should have a user" do
+    check_presence(@vote, :user)
+  end
+
+  test "should have value" do
+    check_presence(@vote, :value)
+  end
+
+
+  test "Should be positive or negative one" do
+      @vote.value = -2
+
+      assert @vote.invalid?, "Vote should be 1 or -1"
+      assert_not_empty @vote.errors[:value]
+  end
+
+  test "should award 10 points to user object" do
+    user = users(:one)
+    question = questions(:one)
+    previous_score = user.score
+    vote = question.votes.create!(:value => 1, :user => user)
+
+    assert_equal 10, user.score - previous_score
+
+  end
+
 end
