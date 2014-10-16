5d2c58f84dd4fd41499d7110c9df563085e71a3e
Kheang Lim <kheang@gmail.com>
2014-10-15 22:46:12 -0400

    Testing vote_count under questions

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -37,8 +37,17 @@ class QuestionTest < ActiveSupport::TestCase
     Vote.create!(up: true, voteable_id: question.id, voteable_type: "Question", user: voter)
 
     question1 = Question.find(question.id)
-    print question1.inspect
     assert_equal 1, question1.vote_count
+  end
+
+  test "should decrement vote count by 1 after down vote" do
+    question = questions(:one)
+    voter = users(:two)
+
+    Vote.create!(voteable_id: question.id, voteable_type: "Question", user: voter)
+
+    question1 = Question.find(question.id)
+    assert_equal -1, question1.vote_count
   end
 
 end
