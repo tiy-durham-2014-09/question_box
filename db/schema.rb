d7bc27403905ade15d548c79830f98a111af497b
GeoffPurdy <geoff_purdy@yahoo.com>
2014-10-16 16:04:49 -0400

    added wireframe pdf to root of project

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20141015202023) do
-=======
 ActiveRecord::Schema.define(version: 20141016173750) do
->>>>>>> 4e14ee86f08850174ea95e735c3cc01ea453a7c1
 
   create_table "answers", force: true do |t|
     t.text     "text"
@@ -48,16 +44,6 @@ ActiveRecord::Schema.define(version: 20141016173750) do
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
-<<<<<<< HEAD
-  create_table "tags", force: true do |t|
-    t.string   "name"
-    t.integer  "question_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "tags", ["question_id"], name: "index_tags_on_question_id"
-=======
   create_table "questions_tags", id: false, force: true do |t|
     t.integer "question_id", null: false
     t.integer "tag_id",      null: false
@@ -66,7 +52,6 @@ ActiveRecord::Schema.define(version: 20141016173750) do
   create_table "tags", force: true do |t|
     t.string "name"
   end
->>>>>>> 4e14ee86f08850174ea95e735c3cc01ea453a7c1
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -78,22 +63,12 @@ ActiveRecord::Schema.define(version: 20141016173750) do
   end
 
   create_table "votes", force: true do |t|
-<<<<<<< HEAD
-    t.boolean  "is_positive"
-    t.integer  "user_id"
-=======
     t.integer  "user_id"
     t.integer  "value"
     t.integer  "voteable_id"
     t.string   "voteable_type"
->>>>>>> 4e14ee86f08850174ea95e735c3cc01ea453a7c1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-<<<<<<< HEAD
-  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
-
-=======
->>>>>>> 4e14ee86f08850174ea95e735c3cc01ea453a7c1
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-
-require 'test_helper'
-
-class TagTest < ActiveSupport::TestCase
-  test "test if tag has a name" do
-    t = tags(:one)
-    assert t.name, "All tags must have a name"
-  end
-
-  test "test if tag has one or more questions" do
-    t = tags(:one)
-    assert t.question_id, "All tags mush have one or more questions"
-  end
-=======
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
   should validate_presence_of :name
   should have_and_belong_to_many :questions
->>>>>>> 4e14ee86f08850174ea95e735c3cc01ea453a7c1
 end
