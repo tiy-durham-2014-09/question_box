ccd26897efdc31b1f2c7606fc2a2ee7b18632c51
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-10-16 10:21:12 -0400

    Converted tests to shoulda

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,43 +1,28 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  setup do
-    @question = Question.new
-  end
+  should validate_presence_of(:title)
+  should validate_presence_of(:text)
+  should validate_presence_of(:user)
 
-  test "should have a title" do
-    check_presence(@question, :title)
-  end
+  should belong_to(:user)
+  should have_many(:answers)
 
-  test "should have text" do
-    check_presence(@question, :text)
-  end
+  context "a question" do
+    should "know if it has a chosen answer" do
+      question = questions(:one)
+      question.answers.update_all(chosen: false)
+      refute question.has_chosen_answer?, "has_chosen_answer? should be false"
 
-  test "should belong to a user" do
-    check_presence(@question, :user)
-  end
-
-  test "should belong to a tag" do
-    check_presence(@question, :tag)
-  end
-
-  test "should know if it has a chosen answer" do
-    question = questions(:one)
-    question.answers.update_all(chosen: false)
-    refute question.has_chosen_answer?, "has_chosen_answer? should be false"
-
-    question.answers.first.update(chosen: true)
-    assert question.has_chosen_answer?, "has_chosen_answer? should be true"
-  end
-
-  test "should know its own score" do
-    question = questions(:one)
-    voter1 = users(:two)
-    voter2 = users(:one)
-    vote1 = question.votes.create!(:value => 1, :user => voter1)
-    vote2 = question.votes.create!(:value => 1, :user => voter2)
-
-    assert_equal 2, question.total_score
-
+      question.answers.first.update(chosen: true)
+      assert question.has_chosen_answer?, "has_chosen_answer? should be true"
+    end
   end
 end
+
+
+
+
+
+
+
