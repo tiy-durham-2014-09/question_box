d3b72b1b9544fce9191c2d578cbff8f83101fced
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-10-16 13:32:24 -0400

    Add users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015224847) do
+ActiveRecord::Schema.define(version: 20141016160032) do
 
   create_table "answers", force: true do |t|
     t.text     "text"
