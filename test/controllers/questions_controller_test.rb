91baf9ef007edd9a16c204b433b5fa3bba40ae01
GeoffPurdy <geoff_purdy@yahoo.com>
2014-10-17 16:35:14 -0400

    created controllers; wrote failing tests for question controller

---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionController::TestCase
+  def valid_question_attributes
+    {title: Faker::Company.catch_phrase + '?',
+     text: Faker::Company.bs,
+     user: 1}
+  end
+
+  def invalid_question_attributes
+    {title: nil,
+     text: nil,
+     user: nil}
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :ok
+  end
+
+  context "POST :create" do
+    context "when I send invalid information" do
+      setup { post :create, { user: invalid_question_attributes } }
+
+      should "re-render the form" do
+        assert_template :new
+      end
+
+      should "instantiate an invalid user object" do
+        assert assigns["question"], "Should have a question"
+        assert assigns["question"].invalid?, "Should have an invalid question"
+      end
+    end
+
+    context "when I send valid information" do
+      should "create a question" do
+        question_attributes = valid_user_attributes
+        post :create, { question: question_attributes }
+
+        assert assigns["question"], "Should have a question"
+        # binding.pry
+        assert assigns["question"].persisted?, "Should have saved question in the DB"
+        assert_equal question_attributes[:name], assigns["question"].name
+      end
+    end
+  end
+
+end
