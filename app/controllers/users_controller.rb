975d563da51671ba2af3e607e2d2c3287f56701d
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-10-18 10:33:07 -0400

    Testing registration works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
+      session[:current_user_id] = @user.id
       # TODO redirect
       render nothing: true, status: :created
     else
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,31 +2,35 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   def valid_user_attributes
-    {name: Faker::Name.name,
-     email: Faker::Internet.email,
-     password: "password",
-     password_confirmation: "password"}
+    { name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: "password",
+      password_confirmation: "password" }
   end
 
   def invalid_user_attributes
-    {name: "",
-     email: "",
-     password: "",
-     password_confirmation: ""}
+    { name: "",
+      email: "",
+      password: "",
+      password_confirmation: "" }
   end
 
-  test "should get new" do
-    get :new
-    assert_response :ok
+  context "GET :new" do
+    setup { get :new }
+
+    should respond_with(:ok)
+    should render_template(:new)
+
+    should "instantiate a user object" do
+      assert assigns[:user], "Should have a user"
+    end
   end
 
   context "POST :create" do
     context "when I send invalid information" do
       setup { post :create, { user: invalid_user_attributes } }
 
-      should "re-render the form" do
-        assert_template :new
-      end
+      should render_template(:new)
 
       should "instantiate an invalid user object" do
         assert assigns["user"], "Should have a user"
@@ -35,14 +39,15 @@ class UsersControllerTest < ActionController::TestCase
     end
 
     context "when I send valid information" do
-      should "create a user" do
-        user_attributes = valid_user_attributes
-        post :create, { user: user_attributes }
+      setup { post :create, { user: valid_user_attributes } }
 
+      should "create a user" do
         assert assigns["user"], "Should have a user"
-        # binding.pry
         assert assigns["user"].persisted?, "Should have saved user in the DB"
-        assert_equal user_attributes[:name], assigns["user"].name
+      end
+
+      should "log in" do
+        assert_equal assigns[:user].id, session[:current_user_id]
       end
     end
   end
