ba45872daf7b6da92bf085aae3db4ecdb314a546
Pete <petermason.byrne@gmail.com>
2014-10-18 13:06:35 -0400

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016173750) do
+ActiveRecord::Schema.define(version: 20141018170355) do
 
   create_table "answers", force: true do |t|
     t.text     "text"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20141016173750) do
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
   add_index "answers", ["user_id"], name: "index_answers_on_user_id"
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.text     "text"
+    t.integer  "commentable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "questions", force: true do |t|
     t.string   "title"
