6fe60c8f637d8e7526fc76dc65b9ff055b63feae
Nick Passarella <nickpassarella@gmail.com>
2014-10-19 12:34:53 -0400

    Added votes routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:new, :create]
   resources :questions
   resources :answers
+  resources :votes, :only => [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
