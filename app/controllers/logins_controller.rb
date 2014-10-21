99b11b2cc0632e713177c4350832b2b9d0cdbbab
Pete <petermason.byrne@gmail.com>
2014-10-21 10:27:59 -0400

    updated with clinton controllers tests

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,23 +1,27 @@
 class LoginsController < ApplicationController
   def show
-    @user = User.find_by(email: params[:email])
+    @login = Login.new
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:current_user_id] = @user.id
-      redirect_to root_path, notice: "Logged in successfully."
+    @login = Login.new(login_params)
+
+    if @login.valid? && @login.authenticated?
+      session[:current_user_id] = @login.user.id
+      redirect_to root_path, success: "Logged in successfully."
     else
-      flash.now[:error] = "Incorrect username or password."
-      render :new
+      render :show
     end
   end
 
   def destroy
     session[:current_user_id] = nil
-    redirect_to root_path, notice: "Logged out successfully."
+    redirect_to root_path, success: "Logged out successfully."
   end
 
+  private
 
+  def login_params
+    params.require(:login).permit(:email, :password)
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,50 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate, only: [:new, :create, :vote]
+  before_action :set_question, only: [:show, :vote]
+
+  def home
+    @question = Question.new
+    @recent_questions = Question.order(created_at: :desc).limit(5)
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def show
+    @answer = @question.answers.build
+  end
+
+  def vote
+    @vote = @question.votes.build(user: current_user, value: params[:vote][:value])
+    if @vote.save
+      redirect_to @question, success: "Your vote was recorded."
+    else
+      redirect_to @question, error: "There was a problem saving your vote."
+    end
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :text)
+  end
 end
