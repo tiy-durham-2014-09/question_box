5aaca511c94cddeda8d44c669a66b8851ebb7405
gregurton <gburt802@gmail.com>
2014-10-24 10:49:34 -0400

    fixed errors in registers controller

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -2,12 +2,15 @@ class RegistersController < ApplicationController
   
   def new
     @register = Register.new
+  end
+
   def update
     @user = @register.user
     if @user.update(register_params)
       @register.update_column(click: true)
       @register.update_column(token: nil)
       redirect_to root_path, success: "Your registration is complete!"
+    end
   end
   
   private
