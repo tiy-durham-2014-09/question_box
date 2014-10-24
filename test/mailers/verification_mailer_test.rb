1182562730604e376a90192a09d53538d76b63bf
David Perry <curiousepic@gmail.com>
2014-10-23 22:12:15 -0400

    Verification email views, passing test

--- test/mailers/verification_mailer_test.rb
+++ test/mailers/verification_mailer_test.rb
@@ -11,8 +11,8 @@ class VerificationMailerTest < ActionMailer::TestCase
     end
     should "send a verification email" do
       mail = VerificationMailer.email_verification(@user)
-      assert_equal ["Welcome to Question Box! Please verify your account"], mail.to
-      assert_equal ["Question.box"], mail.from
+      assert_equal [@user.email], mail.to
+      assert_equal ["noreply@Question.box"], mail.from
       assert_match "Thank you for joining Question Box!  Please click the following link to verify your account and begin asking and answering the most pressing questions of our time.", mail.body.encoded
     end
   end
