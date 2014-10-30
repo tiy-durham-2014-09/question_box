66bf726a78e18dafded69c8dc07dc8b10c383a19
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-10-30 10:11:50 -0400

    Tell VCR to ignore localhost

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,6 +16,8 @@ Capybara.current_driver = :poltergeist
 
 VCR.configure do |c|
   c.cassette_library_dir = 'test/vcr_cassettes'
+  # c.allow_http_connections_when_no_cassette = true
+  c.ignore_localhost = true
   c.hook_into :webmock
 end
 
