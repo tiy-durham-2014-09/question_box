6d6d1bb41e7674323bdb952c405b3213efb3ea75
Noah Holmes <noah.r.holmes@gmail.com>
2014-10-31 15:24:30 -0400

    blah

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,46 +1,7 @@
-var onPage = function (controller, action) {
-  return $("body." + controller + "-" + action).length
-}
-
-var negativeAnswerText = function (count) {
-  var text = "There ";
-  if (count === 1) {
-    text += "is "
-  } else {
-    text += "are ";
-  }
-  text += count;
-  // same as using an if/else statement
-  text += (count === 1 ? " answer " : " answers ");
-  text += "hidden. Click here to show "
-  text += (count === 1 ? "it." : "them.");
-  return text;
-};
-
-var hideNegativeAnswers = function () {
-  var $negativeAnswers = $(".answer[data-negative=true]");
-  var negativeCount = $negativeAnswers.length;
-  if (negativeCount > 0) {
-    $negativeAnswers.hide();
-    // Create new link to show the hidden answers.
-    var linkHTML = "<a href='#'>" + negativeAnswerText(negativeCount) + "</a>";
-    $(linkHTML)
-      .appendTo("#answers")
-      .on('click', function (event) {
-        event.preventDefault();
-        $(this).fadeOut();
-        $negativeAnswers.fadeIn();
-      });
-  }
-}
-
-
 $(document).on('ready', function () {
-  $("#question_tag_list").tagsInput({
-    width: '100%',
+  $("#question_tag_list").tagsInput({width: '100%',
     height: '1rem'});
-    $("#user_tag_list").tagsInput({width: '100%',
-      height: '1rem'});
+
 
   if (onPage("questions", "show")) {
     var question = $(".question-title").text();
@@ -59,9 +20,10 @@ $(document).on('ready', function () {
           .html("<ul></ul>")
           .children("ul")
           .append(links);
+        console.log("completed request");
       }
     });
 
-    hideNegativeAnswers();
+    console.log("made ajax request");
   }
 });
