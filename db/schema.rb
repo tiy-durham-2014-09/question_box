a1364d8857d561a0fc9bfea8acb6af1f1735b21d
gregurton <gburt802@gmail.com>
2014-10-31 18:10:36 -0400

    ran migration to add SMS boolean to question table in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031150750) do
+ActiveRecord::Schema.define(version: 20141031220538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20141031150750) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "vote_count"
+    t.boolean  "SMS"
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
