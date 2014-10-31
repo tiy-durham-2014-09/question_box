018fde1b5619f6ee321d1842e06aacd6d3167bbe
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-10-31 12:19:07 -0400

    profile#show has some user info, working on create and destroy paths

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,7 +16,6 @@ class ProfilesController < ApplicationController
 
 
 	def update
-
 		respond_to do |format|
 			if @profile.update(profile_params)
 				format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
