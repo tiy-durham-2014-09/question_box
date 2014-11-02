a6307898d1d6af519313a27da7da493edaeebd16
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-11-01 23:52:06 -0400

    fixed updating bug on user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,11 @@ class UsersController < ApplicationController
   end
 
   def update
+	  @user = User.find(params[:id])
 	  respond_to do |format|
 		  if @user.update(user_params)
 			  format.html { redirect_to @user, notice: 'Your profile was successfully updated.' }
-			  format.json { render "/profile/show", status: :ok, location: @user }
+			  format.json { render "/questions/home", status: :ok, location: @user }
 		  else
 			  format.html { render :edit }
 			  format.json { render json: @user.errors, status: :unprocessable_entity }
